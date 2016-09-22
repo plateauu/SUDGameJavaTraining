cf1b44e24579c401fbcbf6431c8d1fcdc7cef5cf
plateauu <plateauu@o2.pl>
2016-09-22 18:40:22 +0200

    clean

--- src/main/java/com/plateauu/sudgame/domain/Location.java
+++ src/main/java/com/plateauu/sudgame/domain/Location.java
@@ -13,123 +13,123 @@ import com.plateauu.sudgame.monsters.Npc;
  * TODO: override toString() at Direction and NPC
  */
 public class Location {
-	private String shortDescription;
-	private String longDescription;
-	private Map<Direction, Location> exits;
-	private List<Npc> monsters;
 
-	public Location(String shortDescription, String longDescription) {
-		this.shortDescription = shortDescription;
-		this.longDescription = longDescription;
-		this.exits = new HashMap<>();
-		this.monsters = new ArrayList<>();
-	}
+    private String shortDescription;
+    private String longDescription;
+    private Map<Direction, Location> exits;
+    private List<Npc> monsters;
 
-	public String getShortDescription() {
-		return shortDescription;
-	}
+    public Location(String shortDescription, String longDescription) {
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+        this.exits = new HashMap<>();
+        this.monsters = new ArrayList<>();
+    }
 
-	public String getLongDescription() {
-		return longDescription;
-	}
+    public String getShortDescription() {
+        return shortDescription;
+    }
 
-	public String getDescription() {
-		return "Now, you are at: " + this.shortDescription + "\n"
-				+ this.longDescription + "\n" + "Visible exits: "
-				+ getExitString() + "\n" + "Encountered monsters: "
-				+ getMonsterString();
+    public String getLongDescription() {
+        return longDescription;
+    }
 
-	}
+    public String getDescription() {
+        return "Now, you are at: " + this.shortDescription + "\n"
+                + this.longDescription + "\n" + "Visible exits: "
+                + getExitString() + "\n" + "Encountered monsters: "
+                + getMonsterString();
 
-	private String getExitString() {
-		StringBuilder exitString = new StringBuilder();
-		List<Direction> locationsExits = new ArrayList<>(exits.keySet());
-		Collections.sort(locationsExits);
-		for (Direction direction : locationsExits) {
-			exitString.append(direction.getDirectionDescription());
-			exitString.append(", ");
-		}
-		return exitString.toString();
-	}
+    }
 
-	public String getMonsterString() {
-		if (monsters.isEmpty()) {
-			return "None";
-		} else {
-			StringBuilder monsterString = new StringBuilder();
-			for (Npc monster : monsters) {
-				monsterString.append(monster.getName());
-				monsterString.append(", ");
-			}
-			return monsterString.toString();
-		}
-	}
+    private String getExitString() {
+        StringBuilder exitString = new StringBuilder();
+        List<Direction> locationsExits = new ArrayList<>(exits.keySet());
+        Collections.sort(locationsExits);
+        for (Direction direction : locationsExits) {
+            exitString.append(direction.getDirectionDescription());
+            exitString.append(", ");
+        }
+        return exitString.toString();
+    }
 
-	public boolean isMonsterExists(String name) {
-		boolean isPresent = false;
+    public String getMonsterString() {
+        if (monsters.isEmpty()) {
+            return "None";
+        } else {
+            StringBuilder monsterString = new StringBuilder();
+            for (Npc monster : monsters) {
+                monsterString.append(monster.getName());
+                monsterString.append(", ");
+            }
+            return monsterString.toString();
+        }
+    }
 
-		for (Npc monster : monsters) {
-			if (monster.getName().equalsIgnoreCase(name)) {
-				isPresent = true;
-				break;
-			}
-		}
-		return isPresent;
-	}
+    public boolean isMonsterExists(String name) {
+        boolean isPresent = false;
 
-	public int getMonsterIndex(String name) {
+        for (Npc monster : monsters) {
+            if (monster.getName().equalsIgnoreCase(name)) {
+                isPresent = true;
+                break;
+            }
+        }
+        return isPresent;
+    }
 
-		int indexOfMonster = -1;
+    public int getMonsterIndex(String name) {
 
-		for (Npc monster : monsters) {
-			if (monster.getName().equalsIgnoreCase(name)) {
-				indexOfMonster = monsters.indexOf(monster);
-			}
-		}
-		return indexOfMonster;
-	}
-	
-	public Npc getMonster(String name){
-		
-		int indexOfMonster;
-		indexOfMonster = getMonsterIndex(name);
-		
-		if (indexOfMonster != -1){
-			return monsters.get(indexOfMonster);		
-		} else {
-			return null;
-		}
-	}
-	
-	public void addLocation(Direction exit, Location loc) {
-		this.exits.put(exit, loc);
-	}
+        int indexOfMonster = -1;
 
-	public void addMonster(Npc monster) {
-		this.monsters.add(monster);
-	}
+        for (Npc monster : monsters) {
+            if (monster.getName().equalsIgnoreCase(name)) {
+                indexOfMonster = monsters.indexOf(monster);
+            }
+        }
+        return indexOfMonster;
+    }
 
-	public void removeMonster(Npc monster) {
-		this.monsters.remove(monster);
-	}
-	
-	public Location getNextLocation(Direction direction) {
-		boolean exists = this.exits.containsKey(direction);
-		if (exists) {
-			return this.exits.get(direction);
-		} else {
-			return null;
-		}
+    public Npc getMonster(String name) {
 
-	}
-	
-	public List<Npc> getMonsterList(){
-		return this.monsters;
-	}
-	
-	public Map<Direction, Location> getExtisLists(){
-		return this.exits;
-	}
+        int indexOfMonster;
+        indexOfMonster = getMonsterIndex(name);
 
+        if (indexOfMonster != -1) {
+            return monsters.get(indexOfMonster);
+        } else {
+            return null;
+        }
+    }
+
+    public void addLocation(Direction exit, Location loc) {
+        this.exits.put(exit, loc);
+    }
+
+    public void addMonster(Npc monster) {
+        this.monsters.add(monster);
+    }
+
+    public void removeMonster(Npc monster) {
+        this.monsters.remove(monster);
+    }
+
+    public Location getNextLocation(Direction direction) {
+        boolean exists = this.exits.containsKey(direction);
+        if (exists) {
+            return this.exits.get(direction);
+        } else {
+            return null;
+        }
+
+    }
+
+    public List<Npc> getMonsterList() {
+        return this.monsters;
+    }
+
+    public Map<Direction, Location> getExtisLists() {
+        return this.exits;
+    }
 
 }
